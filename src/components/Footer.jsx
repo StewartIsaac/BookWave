5f954c0541a80a87ceb18733318f61ea9d8b595e
Gabriel <stewarti3@yahoo.com>
2024-10-12 14:25:57 +0100

    update: navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,41 @@
 import React from "react";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col gap-0 content-center justify-center bg-gradient-to-tl from-green-500 to-green-700 text-white text-center p-4 mt-4 md:flex-row md:gap-52 absolute inset-x-0 bottom-0">
-      <p>&copy; {new Date().getFullYear()} Book Library. All rights reserved.</p>
-      <p>Made with ❤️ by <a className="text-bold font-bold font-sans hover:opacity-70" href="http://github.com/stewartisaac/" target="_blank" rel="noopener noreferrer">Gabriel Isaac</a></p>
+    <footer className="absolute inset-x-0 bottom-0  w-full bg-gradient-to-tl from-green-500 to-green-700 text-white p-4">
+      <div className="container flex flex-col items-center justify-center mx-auto md:flex-row md:justify-between">
+        <div>
+          <p className="text-center">
+            &copy; {new Date().getFullYear()} Book Library. All rights reserved.
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-4 items-center self-center md:flex-row">
+          <p>Developed by Gabriel Isaac</p>
+          <div className="flex flex-row gap-4 -mt-3">
+            <a
+              href="https://www.linkedin.com/in/stewartisaac"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:opacity-70"
+            >
+              <FaLinkedin />
+            </a>
+            <a
+              href="https://github.com/StewartIsaac"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white hover:opacity-70"
+            >
+              <FaGithub />
+            </a>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
 
 export default Footer;
+
