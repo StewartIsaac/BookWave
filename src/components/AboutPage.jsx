775829b338c65eb89ada814364668b626dadffc6
Gabriel <stewarti3@yahoo.com>
2024-10-13 21:48:54 +0100

    update: add margin top/btm

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -8,7 +8,7 @@ const AboutPage = () => {
     <>
       <Navbar />
       <div className="container mx-auto p-4 mt-10 text-center max-w-[85%] md:max-w-[75%]">
-        <img src="/logo-g.png" alt="" className="w-14 m-auto md:w-20" />
+        <img src="/logo-g.png" alt="" className="w-14 m-auto mt-20 md:w-20" />
         <h1 className="text-4xl font-bold mb-4">About BookWave</h1>
         <p className="mb-4">
           Welcome to BookWave! This application is designed to help book lovers discover new titles, authors, and genres. With a user-friendly interface and access to a vast library of books, you can easily search for your next read and explore detailed information about each book.
@@ -29,7 +29,7 @@ const AboutPage = () => {
               Hello! I'm <b>Gabriel Isaac</b>, the creator of BookWave. I'm passionate about reading and technology, and I built this app to share my love for books with others. I believe that every great story deserves to be discovered, and I hope you find something wonderful here!
             </p>
           </div>
-          <div className="flex flex-row items-center gap-2">
+          <div className="flex flex-row items-center gap-2 mb-20">
 
             <a
               href="https://www.linkedin.com/in/stewartisaac"
